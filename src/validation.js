743d4feaa0d5cae010ebd1a3511fd94cf1d839bd
Nguyen Hoang Vu <vunh@greenglobal.vn>
2016-10-06 07:55:46 +0700

    apply travis

--- src/validation.js
+++ src/validation.js
@@ -1,32 +1,29 @@
+
 var isNumber = function(n, cb) {
 
   if (isNaN(n)) {
 
-    return cb(new Error( n + ' is not a number'), false);
-  } else {
-
-    return cb(null, true);
+    return cb(new Error(n + ' is not a number'), false);
   }
+  return cb(null, true);
 };
 
-var between = function (min, max, n, cb) {
+var between = function(min, max, n, cb) {
 
   if (isNaN(min) || isNaN(max) || isNaN(n)) {
 
     return cb(new Error('params invaid'), false);
-  } else {
-
-    if (n < min || n > max) {
-
-      return cb(new Error( n + ' not between ' + min + ' and ' + max), false);
-    } else {
-
-      return cb(null, true);
-    }
   }
-}
+
+  if (n < min || n > max) {
+
+    return cb(new Error(n + ' not between ' + min + ' and ' + max), false);
+  }
+
+  return cb(null, true);
+};
 
 module.exports = {
-  isNumber: isNumber,
-  between: between
-}
+  isNu: isNumber,
+  bet: between
+};
--- test/validation.js
+++ test/validation.js
@@ -1,13 +1,13 @@
 var expect = require('chai').expect;
 var validation = require('../src/validation.js');
 
-describe('validation', function () {
+describe('validation', function() {
 
-  describe('isNumber()', function () {
+  describe('isNumber()', function() {
 
     it('return error when not number', function(done) {
 
-      validation.isNumber('abc', function(err, rs) {
+      validation.isNu('abc', function(err, rs) {
 
         expect(err.message).to.equal('abc is not a number');
         expect(rs).equal(false);
@@ -17,44 +17,44 @@ describe('validation', function () {
 
     it('return true when number', function(done) {
 
-      validation.isNumber(4, function(err, rs) {
+      validation.isNu(4, function(err, rs) {
         expect(err).to.equal(null);
         expect(rs).to.equal(true);
         done();
-      })
-    })
+      });
+    });
   });
 
   describe('between()', function() {
 
     it('return false if param invaid', function(done) {
 
-      validation.between('a', 1, 4, function(err, rs) {
+      validation.bet('a', 1, 4, function(err, rs) {
 
         expect(err.message).to.equal('params invaid');
         expect(rs).to.equal(false);
         done();
-      })
-    })
+      });
+    });
 
     it('return false when n invaid', function(done) {
 
-      validation.between(2, 5, 1, function(err, rs) {
+      validation.bet(2, 5, 1, function(err, rs) {
 
         expect(err.message).to.equal('1 not between 2 and 5');
         expect(rs).to.equal(false);
         done();
-      })
+      });
     });
 
     it('return true when min is 2 max is 5 n is 3', function(done) {
 
-      validation.between(2, 5, 3, function(err, rs) {
+      validation.bet(2, 5, 3, function(err, rs) {
 
         expect(err).to.equal(null);
         expect(rs).to.equal(true);
         done();
-      })
-    })
+      });
+    });
   });
-})
+});
